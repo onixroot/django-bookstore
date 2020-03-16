4255fc81fcbc78f3b00031f44e49c90563f85042
Onix <onixroot@gmail.com>
2020-03-16 17:02:02 +0300

    + docker_compose_prod.yml config

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     # Django admin
-    path('admin/', admin.site.urls),
+    path('anything-but-admin/', admin.site.urls),
 
     # User management
     path('accounts/', include('allauth.urls')),
@@ -15,3 +15,9 @@ urlpatterns = [
     path('books/', include('books.urls')),
     path('orders/', include('orders.urls')), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
